bc3a9b7120ce9f26f78d05cef687cd4de437ca0e
rakib13x <rakib13x@gmail.com>
2024-01-29 02:36:29 +0600

    interface  User in  types.ts and type MessageResonse in api-types.ts added

---
+++ src/redux/api/userAPI.ts
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { server } from "../store";
+import { MessageResponse } from "../../types/api-types";
+import { User } from "../../types/types";
+
+export const userAPI = createApi({
+  reducerPath: "userApi",
+  // /api/v1/user/new
+  baseQuery: fetchBaseQuery({ baseUrl: `${server}/api/v1/user/` }),
+  endpoints: (builder) => ({
+    login: builder.mutation<MessageResponse, User>({
+      query: (user) => ({
+        url: "new",
+        method: "POST",
+        body: user,
+      }),
+    }),
+  }),
+});
